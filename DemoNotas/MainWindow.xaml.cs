a5cad70c22eb89616eee29ff915dbf92c0d6e9f7
ima98 <ilavinlertxundi@gmail.com>
2023-10-01 20:56:32 +0200

    cliente Nota

---
+++ DemoNotas/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DemoNotas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class Nota
+    {
+        public int Num { get; set; }
+        public string? Texto { get; set; }
+
+        public override string ToString()
+        {
+            return Texto;
+        }
+
+        public Nota(int num, string texto)
+        {
+            Num = num;
+            Texto = texto;
+        }
+
+
+    }
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void ButtonSave(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var response = await RestConsumer.Save(textblock.Text, (combobox.Items.Count + 1));
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Error, {err.Message}");
+            }
+        }
+
+        private async void ButtonDelete(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                Nota cbp = (Nota)combobox.SelectedItem;
+                var response = await RestConsumer.Delete(cbp.Num);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Error, {err.Message}");
+            }
+
+        }
+
+        private async void ButtonGetAll(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var response = await RestConsumer.GetAll();
+                List<Nota> cbp = new List<Nota>();
+                for (int i = 0; i < response.Count; i++)
+                {
+                    var nt = new Nota(response[i].Num, response[i].Texto);
+                    cbp.Add(nt);
+
+                }
+                combobox.DisplayMemberPath = "Texto";
+                combobox.SelectedValuePath = "Num";
+                Console.WriteLine(cbp);
+                combobox.ItemsSource = cbp;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Error, {err.Message}");
+            }
+        }
+
+
+    }
+}
---
+++ DemoNotas/RestConsumer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoNotas
+{
+    public static class RestConsumer
+    {
+        private static readonly string baseURL = "http://localhost:3001/";
+        public static async Task<List<Nota>> GetAll()
+        {
+            try
+            {
+
+                List<Nota> data = new List<Nota>();
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(baseURL + "notas"))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            Console.WriteLine(content);
+                            data = await content.ReadFromJsonAsync<List<Nota>>();
+                            if (data != null)
+                            {
+                                return data;
+                            }
+                        }
+                    }
+
+                }
+                data = new List<Nota>();
+                return data;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Servicio no disponible");
+                return null;
+            }
+
+        }
+
+        public static async Task<string> Save(string texto, int num)
+        {
+            try
+            {
+                var inputData = new Dictionary<string, string>
+        {
+            {"texto", texto},
+            {"num", num.ToString()}
+        };
+                var input = new FormUrlEncodedContent(inputData);
+
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.PostAsync(baseURL + "newNota", input))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+
+                        }
+                    }
+                }
+                return "okey";
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Servicio no disponible");
+                return null;
+            }
+        }
+
+
+        public static async Task<string> Delete(int num)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.DeleteAsync(baseURL + "delete" + "/" + num.ToString()))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+
+                        }
+                    }
+                }
+                return "okey";
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Servicio no disponible");
+                return null;
+            }
+
+        }
+    }
+
+}
